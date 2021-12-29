0793b1940cd2ae0cddaeb11ae1d0e1021d3b3287
TheCryptoMonkey <93091398+TheCryptoMonkey@users.noreply.github.com>
2021-12-29 17:10:22 -0600

    moved import statements outside of main function

--- Graphics_Card_Web_Scraping.py
+++ Graphics_Card_Web_Scraping.py
@@ -1,12 +1,12 @@
+import urllib.request
+import csv
+from lxml import etree
+from selenium import webdriver
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+    
 def main():
-
-    import urllib.request
-    import csv
-    from lxml import etree
-    from selenium import webdriver
-    from urllib.request import urlopen as uReq
-    from bs4 import BeautifulSoup as soup
-
+    
     default_url = 'https://whattomine.com/coins?aq_480=1&aq_68xt=1&a_68xt=true&aq_68=0&aq_67xt=0&aq_66xt=0&aq_vii=0&aq_5700xt=0&aq_5700=0&aq_5600xt=0&aq_vega64=0&aq_vega56=0&aq_3090=0&aq_3080Ti=0&aq_3080=0&aq_3080L=0&aq_3070Ti=0&aq_3070=0&aq_3070L=0&aq_3060Ti=0&aq_3060TiL=0&aq_3060=0&aq_3060L=0&aq_55xt8=0&aq_580=0&aq_570=0&aq_470=0&aq_fury=0&aq_380=0&aq_2080Ti=0&aq_2080=0&aq_2070=0&aq_2060=0&aq_166s=0&aq_1660Ti=0&aq_1660=0&aq_1080Ti=0&aq_1080=0&aq_1070Ti=0&aq_1070=0&aq_10606=0&aq_1050Ti=0&eth=true&factor%5Beth_hr%5D=64.00&factor%5Beth_p%5D=150.00&e4g=true&factor%5Be4g_hr%5D=64.00&factor%5Be4g_p%5D=150.00&zh=true&factor%5Bzh_hr%5D=90.00&factor%5Bzh_p%5D=150.00&cnh=true&factor%5Bcnh_hr%5D=0.00&factor%5Bcnh_p%5D=0.00&cng=true&factor%5Bcng_hr%5D=3300.00&factor%5Bcng_p%5D=180.00&cnr=true&factor%5Bcnr_hr%5D=0.00&factor%5Bcnr_p%5D=0.00&cnf=true&factor%5Bcnf_hr%5D=0.00&factor%5Bcnf_p%5D=0.00&eqa=true&factor%5Beqa_hr%5D=0.00&factor%5Beqa_p%5D=0.00&cc=true&factor%5Bcc_hr%5D=7.00&factor%5Bcc_p%5D=140.00&cr29=true&factor%5Bcr29_hr%5D=0.00&factor%5Bcr29_p%5D=0.00&ct31=true&factor%5Bct31_hr%5D=2.20&factor%5Bct31_p%5D=150.00&ct32=true&factor%5Bct32_hr%5D=0.90&factor%5Bct32_p%5D=150.00&eqb=true&factor%5Beqb_hr%5D=36.00&factor%5Beqb_p%5D=150.00&rmx=true&factor%5Brmx_hr%5D=0.00&factor%5Brmx_p%5D=0.00&ns=true&factor%5Bns_hr%5D=0.00&factor%5Bns_p%5D=0.00&al=true&factor%5Bal_hr%5D=110.00&factor%5Bal_p%5D=150.00&ops=true&factor%5Bops_hr%5D=44.00&factor%5Bops_p%5D=190.00&eqz=true&factor%5Beqz_hr%5D=0.00&factor%5Beqz_p%5D=0.00&zlh=true&factor%5Bzlh_hr%5D=0.00&factor%5Bzlh_p%5D=0.00&kpw=true&factor%5Bkpw_hr%5D=33.00&factor%5Bkpw_p%5D=200.00&ppw=true&factor%5Bppw_hr%5D=0.00&factor%5Bppw_p%5D=0.00&x25x=true&factor%5Bx25x_hr%5D=0.00&factor%5Bx25x_p%5D=0.00&mtp=true&factor%5Bmtp_hr%5D=0.00&factor%5Bmtp_p%5D=0.00&vh=true&factor%5Bvh_hr%5D=0.00&factor%5Bvh_p%5D=0.00&factor%5Bcost%5D=0.1&sort=Profitability24&volume=0&revenue=24h&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=binance&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bitforex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=coinex&factor%5Bexchanges%5D%5B%5D=dove&factor%5Bexchanges%5D%5B%5D=exmo&factor%5Bexchanges%5D%5B%5D=gate&factor%5Bexchanges%5D%5B%5D=graviex&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=hotbit&factor%5Bexchanges%5D%5B%5D=ogre&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=stex&dataset=&commit=Calculate'
 
     nvdia_url_list = []
